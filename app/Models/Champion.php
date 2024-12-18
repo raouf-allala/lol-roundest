3a9becbcb39eccb7bcfc6dba672627a2ae7fff62
raouf-allala <96212241+raouf-allala@users.noreply.github.com>
2024-12-18 01:36:55 +0100

    finished the voting and results

--- app/Models/Champion.php
+++ app/Models/Champion.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Champion extends Model
 {
     protected $fillable = ['name', 'title','blurb'];
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute(){
+        return "https://ddragon.leagueoflegends.com/cdn/14.24.1/img/champion/{$this->name}.png";
+    }
+
+    public static function getTwoChampions(){
+        return self::inRandomOrder()->limit(2)->get();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Livewire\Vote::class);
+Route::get('/results', \App\Livewire\Results::class);
